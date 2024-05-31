18d38b2b1322d13d2c6eadca9b92da239ed9856f
Nicolas Meuwly <nicomeuwly@gmail.com>
2024-05-31 12:23:33 +0200

    try to fix missing dependencies on vercel part 2

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -7,7 +7,9 @@ puppeteer.use(StealthPlugin());
 
 export async function GET() {
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
 
     const endpoint =
